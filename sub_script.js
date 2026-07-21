6fbaad5500c86ccdb96b3cb04784f7420424ff35
ubob_kjm <jmkim@ubob.com>
2026-07-21 13:02:36 +0900

    Update project slider UI

--- sub_script.js
+++ sub_script.js
@@ -1,18 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
-
     new Swiper(".swiper", {
         loop: true,
-
         navigation: {
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev",
         },
-
         keyboard: {
             enabled: true,
         },
-
         speed: 700,
     });
+});
 
+new Swiper(".swiper", {
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    keyboard: {
+        enabled: true,
+    },
+    autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+        pauseOnMouseEnter: true,
+    },
+    speed: 1000,
+    grabCursor: true,
 });
